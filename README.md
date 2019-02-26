ab7b88f1a38624ec457ad1132913f679a172a864
guitarrapc <3856350+guitarrapc@users.noreply.github.com>
2019-02-26 20:55:15 +0900

    feat: initial commit

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-Base64Url
+## Base64Url
+
+Converting to, and from, [base64url](https://en.wikipedia.org/wiki/Base64#RFC_4648)
+
+## Install
+
+TBD
+
+[TODO: NuGet]
+
+## Usage
+
+### Encode
+
+```csharp
+TBD
+```
+
+### Decode
+
+```csharp
+TBD
+```
+
+### FromBase64
+
+```csharp
+TBD
+```
+
+### ToBase64
+
+```csharp
+TBD
+```
+
+### PadString
+
+```csharp
+TBD
+```
+
+## License
+
+MIT
---
+++ src/Base64Url/Base64Url.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text;
+
+namespace Base64Url
+{
+    public static class Base64Url
+    {
+        private static Encoding defaultEncoding = new UTF8Encoding(false);
+
+        public static string Encode(string text)
+        {
+            return Encode(text, defaultEncoding);
+        }
+
+        public static string Encode(string text, Encoding enc)
+        {
+            var bytes = enc.GetBytes(FromBase64(text));
+            return Convert.ToBase64String(bytes);
+        }
+
+        public static string Decode(string text)
+        {
+            return Decode(text, defaultEncoding);
+        }
+
+        public static string Decode(string text, Encoding enc)
+        {
+            var base64 = Convert.FromBase64String(ToBase64(text));
+            return enc.GetString(base64);
+        }
+
+        public static string ToBase64(string base64Url)
+        {
+            return PadString(base64Url)
+                .Replace("-", "+")
+                .Replace("_", "/");
+        }
+
+        public static string FromBase64(string base64)
+        {
+            return base64.Replace("=", "")
+                .Replace("+", "-")
+                .Replace("/", "_");
+        }
+
+        public static string PadString(string text)
+        {
+            var segment = 4;
+            var length = text.Length;
+            var diff = length % segment;
+
+            if (diff == 0) return text;
+
+            var padLength = segment - diff;
+            while (padLength-- != 0)
+            {
+                text += "=";
+            }
+
+            return text;
+        }
+    }
+}
