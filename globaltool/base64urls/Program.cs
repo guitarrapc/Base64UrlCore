ced2bf5fa55761cb3181c9c265b8f0ef02042b3e
Ikiru Yoshizaki <guitarrapc@users.noreply.github.com>
2019-03-18 01:40:48 +0900

    refactor: replace base64urls and base64urls.batch

--- globaltool/base64urls/Program.cs
+++ globaltool/base64urls/Program.cs
@@ -1,98 +1,64 @@
-﻿using System;
-using System.Linq;
+﻿using MicroBatchFramework;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Reflection;
+using System.Threading.Tasks;
 
 namespace Base64UrlCore.Tool
 {
     class Program
     {
-        enum Command
+        static async Task Main(string[] args) => 
+            await new HostBuilder().RunBatchEngineAsync<Base64Batch>(args);
+    }
+
+    public class Base64Batch : BatchBase
+    {
+        private readonly ILogger<BatchEngine> logger;
+        public Base64Batch(ILogger<BatchEngine> logger)
         {
-            none, // unsupported command
-            decode,
-            encode,
-            escape,
-            unescape,
+            this.logger = logger;
         }
+        [Command("encode", "encode input string to base64url")]
+        public void Encode([Option(0)]string input) => logger.LogInformation(Base64Url.Encode(input));
 
-        static void Main(string[] args)
-        {
-            // version -> help -> invalid -> command
-            if (HasFlag(args, "v", "version"))
-            {
-                ShowVersion();
-                return;
-            }
-            if (HasFlag(args, "h", "help"))
-            {
-                ShowHelp();
-                return;
-            }
-            if (!IsValidArguments(args))
-            {
-                ShowHelp();
-                return;
-            }
+        [Command("decode", "decode input base64url to string")]
+        public void Decode([Option(0)]string input) => logger.LogInformation(Base64Url.Decode(input));
 
-            var command = FetchCommand(args[0]);
-            var input = args[1];
-            try
-            {
-                switch (command)
-                {
-                    case Command.none:
-                        ShowHelp();
-                        return;
-                    case Command.decode:
-                        Console.WriteLine(Base64Url.Decode(input));
-                        return;
-                    case Command.encode:
-                        Console.WriteLine(Base64Url.Encode(input));
-                        return;
-                    case Command.escape:
-                        Console.WriteLine(Base64Url.Escape(input));
-                        return;
-                    case Command.unescape:
-                        Console.WriteLine(Base64Url.Unescape(input));
-                        return;
-                    default:
-                        throw new NotImplementedException($"{command} is not impremented.");
-                }
-            }
-            catch (FormatException e)
-            {
-                Console.WriteLine($"failed decode input. {e.Message}");
-            }
-        }
+        [Command("escape", "escape base64 to base64url")]
+        public void Escape([Option(0)]string input) => logger.LogInformation(Base64Url.Escape(input));
 
-        static bool HasFlag(string[] args, string shortName, string longName) => args.Length != 0 && (args.Contains("-" + shortName) || args.Contains("-" + longName) || args.Contains("--" + longName));
-        static bool IsValidArguments(string[] args) => args.Length == 2;
-        static Command FetchCommand(string actual) => Enum.TryParse<Command>(actual, ignoreCase: true, result: out var command)
-                ? command
-                : Command.none;
+        [Command("unescape", "unescape base64url to base64")]
+        public void Unescape([Option(0)]string input) => logger.LogInformation(Base64Url.Unescape(input));
 
         /// <summary>
-        /// -v -version --version
+        /// Provide unix style command argument: -version --version -v + version command
         /// </summary>
-        static void ShowVersion()
+        [Command(new[] { "version", "-v", "-version", "--version" }, "show version")]
+        public void Version()
         {
             var version = Assembly.GetEntryAssembly()
                 .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
                 .InformationalVersion
                 .ToString();
-            Console.WriteLine($"base64urls v{version}");
+            logger.LogInformation($"base64urls v{version}");
         }
 
         /// <summary>
-        /// -h -help --help
+        /// Provide unix style command argument: -help --help -h + override default help / list
         /// </summary>
-        static void ShowHelp()
+        /// <remarks>
+        /// Also override default help. no arguments execution will fallback to here.
+        /// </remarks>
+        [Command(new[] { "help", "list", "-h", "-help", "--help" }, "show help")]
+        public void Help()
         {
-            Console.WriteLine("Usage: base64urls [-version] [-help] [decode|encode|escape|unescape] [args]");
-            Console.WriteLine("E.g., run this: base64urls decode QyMgaXMgYXdlc29tZQ==");
-            Console.WriteLine("E.g., run this: base64urls encode \"C# is awesome.\"");
-            Console.WriteLine("E.g., run this: base64urls escape \"This+is/goingto+escape==\"");
-            Console.WriteLine("E.g., run this: base64urls unescape \"This-is_goingto-escape\"");
+            logger.LogInformation("Usage: base64urls [-version] [-help] [decode|encode|escape|unescape] [args]");
+            logger.LogInformation("E.g., run this: base64urls decode QyMgaXMgYXdlc29tZQ==");
+            logger.LogInformation("E.g., run this: base64urls encode \"C# is awesome.\"");
+            logger.LogInformation("E.g., run this: base64urls escape \"This+is/goingto+escape==\"");
+            logger.LogInformation("E.g., run this: base64urls unescape \"This-is_goingto-escape\"");
         }
     }
 }
